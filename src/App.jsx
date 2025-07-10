0fac8d78e4dbe5948d0d63374054d975b9b01e0f
Alejandro-Cuesta <janittoss@hotmail.com>
2025-07-10 20:25:36 +0200

    fix:Routes App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,24 @@
-import Header from './components/Header/Header'
-import Footer from './components/Footer/Footer'
+import Header from './components/Header/Header';
+import Footer from './components/Footer/Footer';
 import './App.css';
 import { Routes, Route, Navigate } from 'react-router-dom';
 import MapPage from './pages/MapPage/MapPage';
 import HomePage from './pages/HomePage/HomePage';
 import AdoptPage from './pages/AdoptPage/AdoptPage';
-import {Routes, Route } from 'react-router-dom'
-import Layout from './components/Layout/Layout'
-import Test from './components/Test/Test'
-import './App.css'
+import Layout from './components/Layout/Layout';
+import Test from './components/Test/Test';
 
 function App() {
-
   return (
     <Routes>
       <Route path="/" element={<Layout />}>
-       {/* Aquí es donde irán las páginas que se mostrarán en el <Outlet/> */}
-        {/* <Route index element={<HomePage />} />
-        <Route path="adopt" element={<AdoptPage />} /> */}
-      
+        <Route index element={<HomePage />} />
+        <Route path="adopt" element={<AdoptPage />} />
+        <Route path="mapa" element={<MapPage />} />
       </Route>
-      </Routes>
+      <Route path="*" element={<Navigate to="/" />} />
+    </Routes>
   );
 }
 
-export default App
+export default App;
