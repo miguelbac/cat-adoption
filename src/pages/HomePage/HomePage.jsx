5551d0f85c663216c65c0cad1873a8b43b29ab65
miguelbac <mbacebal@gmail.com>
2025-07-10 12:10:31 +0200

    Home Page updated

---
+++ src/pages/HomePage/HomePage.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import "./HomePage.css";
+import bgImage from "../../assets/background.png";
+import CatSlider from "../../components/CatSlider/CatSlider";
+
+
+function HomePage() {
+  return (
+    <div className="home-page" style={{ backgroundImage: `url(${bgImage})` }}>
+      <div className="content">
+        <CatSlider/>
+      </div>
+    </div>
+  );
+}
+
+export default HomePage;
