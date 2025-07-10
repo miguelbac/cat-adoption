df69f84485aabe3051be39a807e7c9d484f33180
Alejandro-Cuesta <janittoss@hotmail.com>
2025-07-10 10:45:43 +0200

    fix:Merge rama 4

--- src/App.jsx
+++ src/App.jsx
@@ -2,18 +2,19 @@ import Header from './components/Header/Header'
 import Footer from './components/Footer/Footer'
 import Test from './components/Test/Test'
 import './App.css'
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
+import MapPage from './pages/MapPage/MapPage';
 
 function App() {
 
   return (
-    <div className="app">
-      <Header />
-      <main className="app__main">
-        {/* El contenido principal irá aquí */}
-      </main>
-      <Footer />
-    </div>
-  )
+    <Router>
+      <Routes>
+        <Route path="/" element={<Navigate to="/mapa" />} />
+        <Route path="/mapa" element={<MapPage />} />
+      </Routes>
+    </Router>
+  );
 }
 
 export default App
