9000ff2e2c20b3fb30080d5953233dd6658553be
Alejandro-Cuesta <janittoss@hotmail.com>
2025-07-10 11:14:41 +0200

    fix:duplicado Ruta

--- src/App.jsx
+++ src/App.jsx
@@ -2,19 +2,21 @@ import Header from './components/Header/Header'
 import Footer from './components/Footer/Footer'
 import Test from './components/Test/Test'
 import './App.css'
-import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
+import { Routes, Route, Navigate } from 'react-router-dom';
 import MapPage from './pages/MapPage/MapPage';
 
 function App() {
 
   return (
-    <Router>
+    <>
+    <Header></Header>
       <Routes>
         <Route path="/" element={<Navigate to="/mapa" />} />
         <Route path="/mapa" element={<MapPage />} />
       </Routes>
-    </Router>
-  );
+    <Footer></Footer>
+    </>
+  )
 }
 
 export default App
